b26a0309ed5bb94fb9b635151b6054e898d9c567
Daniel Sokolowski <github@d.sokolowski.org>
2019-12-13 18:03:23 +0100

    Dirty fix: TCP connect retry with backoff

--- scala-loci-communicator-tcp/jvm/src/main/scala/loci/communicator/tcp/TCPConnector.scala
+++ scala-loci-communicator-tcp/jvm/src/main/scala/loci/communicator/tcp/TCPConnector.scala
@@ -5,7 +5,7 @@ package tcp
 import scala.util.Success
 import scala.util.Failure
 import scala.util.control.NonFatal
-import java.net.Socket
+import java.net.{ConnectException, Socket}
 
 private class TCPConnector(
   host: String, port: Int, properties: TCP.Properties)
@@ -13,12 +13,24 @@ private class TCPConnector(
 
   protected def connect(connectionEstablished: Connected[TCP]): Unit = {
     new Thread() {
+      var retryCounter = 5
+      val backoffMs = 1000L
+
       override def run =
         try TCPHandler handleConnection (
           new Socket(host, port), properties, TCPConnector.this, { connection =>
             connectionEstablished set Success(connection)
           })
         catch {
+          case exception: ConnectException  =>
+            println(s"Connection failed, retry in $backoffMs ms")
+            if (retryCounter > 0) {
+              retryCounter -= 1
+              Thread.sleep(backoffMs)
+              run
+            }
+            else
+              connectionEstablished set Failure(exception)
           case NonFatal(exception) =>
             connectionEstablished set Failure(exception)
         }
